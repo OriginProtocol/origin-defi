3c044d3800bf2b3d1841f1d6ba7ba15041442c1d
Rafael Ugolini <rafael.ugolini@gmail.com>
2023-07-28 16:56:24 +0200

    fix: remove team from danger

--- dangerfile.ts
+++ dangerfile.ts
@@ -3,10 +3,4 @@ import dangerGithubNotion from 'danger-plugin-danger-github-notion';
 dangerGithubNotion({
   dbTasksId: '00c83143-d518-49a1-9a6c-99ca369a0ce6',
   taskPrefix: 'TAS',
-  teams: [
-    {
-      org: 'OriginProtocol',
-      team_slug: 'core',
-    },
-  ],
 });
