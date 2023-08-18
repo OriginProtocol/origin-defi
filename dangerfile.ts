cab2457a1114d978293b911994e0cfca96e35557
Rafael Ugolini <rafael.ugolini@gmail.com>
2023-08-18 18:14:30 +0200

    feat: add danger

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,4 +1,4 @@
-import dangerGithubNotion from 'danger-plugin-danger-github-notion';
+import dangerGithubNotion from 'danger-plugin-github-notion';
 
 dangerGithubNotion({
   dbTasksId: 'e53909e5-e202-4c3f-9b45-0c63b8f6dd8e',
