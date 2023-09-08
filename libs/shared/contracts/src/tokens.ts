d1d9d155313c9ecc637fc602968a94dc4d41099e
toniocodo <antoine@originprotocol.com>
2023-09-08 09:14:08 +0200

    feat: update sfrxEth ABI

--- libs/shared/contracts/src/tokens.ts
+++ libs/shared/contracts/src/tokens.ts
@@ -1,6 +1,8 @@
 import { erc20ABI } from 'wagmi';
 import { mainnet } from 'wagmi/chains';
 
+import { sfrxethABI } from './abis/sfrxeth';
+
 export const tokens = {
   mainnet: {
     ETH: {
@@ -155,7 +157,7 @@ export const tokens = {
     sfrxETH: {
       address: '0xac3E018457B222d93114458476f3E3416Abbe38F',
       chainId: mainnet.id,
-      abi: erc20ABI,
+      abi: sfrxethABI,
       name: 'Staked Frax Ether',
       icon: ' /images/tokens/sfrxETH.svg',
       decimals: 18,
