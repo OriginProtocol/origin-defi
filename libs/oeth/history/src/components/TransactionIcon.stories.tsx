46adb601af7a8a17ebcc3b53d44a7268fe5c66ef
dcodes05 <danutcodes@gmail.com>
2023-09-09 21:08:57 +0200

    feat: history transaction icons

---
+++ libs/oeth/history/src/components/TransactionIcon.stories.tsx
@@ -0,0 +1,41 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { TransactionIcon } from './TransactionIcon';
+
+const meta: Meta<typeof TransactionIcon> = {
+  component: TransactionIcon,
+  title: 'History/TransactionIcon',
+  args: {
+    type: 'yield',
+  },
+};
+
+export default meta;
+
+export const Yield: StoryObj<typeof TransactionIcon> = {};
+
+export const SmallScreen: StoryObj<typeof TransactionIcon> = {
+  parameters: {
+    viewport: {
+      defaultViewport: 'mobile1',
+    },
+  },
+};
+
+export const Sent: StoryObj<typeof TransactionIcon> = {
+  args: {
+    type: 'sent',
+  },
+};
+
+export const Received: StoryObj<typeof TransactionIcon> = {
+  args: {
+    type: 'received',
+  },
+};
+
+export const Swap: StoryObj<typeof TransactionIcon> = {
+  args: {
+    type: 'swap',
+    tokenIcon: 'https://app.oeth.com/images/currency/reth-icon-small.png',
+  },
+};
