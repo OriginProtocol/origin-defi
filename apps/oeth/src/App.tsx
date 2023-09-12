e08dc266eaf4b049c899f0208a4e1a7150034444
dcodes05 <danutcodes@gmail.com>
2023-09-12 20:22:11 +0200

    fix: merge conflict

--- apps/oeth/src/App.tsx
+++ apps/oeth/src/App.tsx
@@ -1,33 +1,32 @@
-import { Container, Stack } from '@mui/material';
-import { HistoryView } from '@origin/oeth/history';
-import { SwapView } from '@origin/oeth/swap';
-import { Route, Routes } from 'react-router-dom';
+import { Container, CssBaseline, Stack } from '@mui/material';
+import { Outlet } from 'react-router-dom';
 
 import { Topnav } from './components/Topnav';
 
-export function App() {
+export const App = () => {
   return (
-    <Stack>
-      <Topnav />
-      <Container
-        sx={{
-          mt: {
-            xs: 3,
-            md: 5,
-            paddingInline: {
-              xs: 2,
-              md: 0,
+    <>
+      <CssBaseline />
+      <Stack>
+        <Topnav />
+        <Container
+          sx={{
+            mt: {
+              xs: 3,
+              md: 5,
+              paddingInline: {
+                xs: 2,
+                md: 0,
+              },
             },
-          },
-        }}
-        maxWidth="sm"
-      >
-        <Routes>
-          <Route index element={<SwapView />} />
-          <Route path="history" element={<HistoryView />} />
-          <Route path="swap" element={<SwapView />} />
-        </Routes>
-      </Container>
-    </Stack>
+          }}
+          maxWidth="sm"
+        >
+          <Stack mt={3}>
+            <Outlet />
+          </Stack>
+        </Container>
+      </Stack>
+    </>
   );
-}
+};
