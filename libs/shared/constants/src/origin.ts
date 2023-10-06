c1352753092847af7f5334937054e3b1955f494a
toniocodo <antoine@originprotocol.com>
2023-10-06 10:04:30 +0200

    feat: add constant libs and static contacts

---
+++ libs/shared/constants/src/origin.ts
@@ -0,0 +1,19 @@
+export const ANALYTICS_URL = 'https://analytics.ousd.com';
+export const DOCS_URL = 'https://docs.ousd.com';
+export const JOBS_URL = 'https://angel.co/company/originprotocol/jobs';
+export const TERMS_URL = 'https://originprotocol.com/tos';
+export const PRIVACY_URL = 'https://originprotocol.com/privacy';
+export const GITHUB_URL = 'https://github.com/originprotocol';
+export const DISCORD_URL = 'https://discord.gg/jyxpUSe';
+export const TELEGRAM_URL = 'https://t.me/originprotocol';
+export const WECHAT_URL = 'https://u.wechat.com/MPuvZYXixbeMimCix6GzRcw';
+export const REDDIT_URL = 'https://www.reddit.com/r/originprotocol';
+export const WEIBO_URL = 'https://weibo.com/originprotocol';
+export const FACEBOOK_URL = 'https://www.facebook.com/originprotocol';
+export const TWITTER_URL = 'https://twitter.com/originprotocol';
+export const MEDIUM_URL = 'https://medium.com/originprotocol';
+export const YOUTUBE_URL = 'http://youtube.com/originprotocol';
+export const INSTAGRAM_URL = 'http://instagram.com/originprotocol';
+export const EMAIL_LIST_URL =
+  'https://www.originprotocol.com/mailing-list/join';
+export const AIRDROP_URL = 'https://governance.ousd.com/claim';
