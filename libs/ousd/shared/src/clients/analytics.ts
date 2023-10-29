8423ffbbeebf9a741740956f18af44e858d869ba
toniocodo <antoine@originprotocol.com>
2023-10-29 14:25:47 +0100

    feat: use Swap track event types

--- libs/ousd/shared/src/clients/analytics.ts
+++ libs/ousd/shared/src/clients/analytics.ts
@@ -4,59 +4,20 @@ import { Analytics } from 'analytics';
 import { map } from 'ramda';
 import { formatEther } from 'viem';
 
+import type { SwapTrackEvent } from '@origin/shared/providers';
 import type { HexAddress } from '@origin/shared/utils';
 
 export type TrackEvent =
-  | { name: 'approve_started'; approval_token: string }
-  | { name: 'approve_complete'; approval_token: string }
-  | { name: 'approve_failed'; approval_token: string; approve_error: string }
-  | { name: 'approve_rejected'; approval_token: string }
+  | SwapTrackEvent
   | { name: 'redeem_started'; redeem_amount: bigint }
   | { name: 'redeem_complete'; redeem_amount: bigint }
   | { name: 'redeem_failed'; redeem_amount: bigint; redeem_error: string }
   | { name: 'redeem_rejected'; redeem_amount: bigint }
-  | {
-      name: 'swap_started';
-      swap_route: string;
-      swap_token: string;
-      swap_to: string;
-      swap_amount: bigint;
-    }
-  | {
-      name: 'swap_complete';
-      swap_route: string;
-      swap_token: string;
-      swap_to: string;
-      swap_amount: bigint;
-    }
-  | {
-      name: 'swap_failed';
-      swap_route: string;
-      swap_token: string;
-      swap_to: string;
-      swap_amount: bigint;
-      swap_error: string;
-    }
-  | {
-      name: 'swap_rejected';
-      swap_route: string;
-      swap_token: string;
-      swap_to: string;
-      swap_amount: bigint;
-    }
-  | { name: 'change_input_amount'; change_amount_to: bigint }
-  | { name: 'change_input_currency'; change_input_to: string }
-  | { name: 'change_output_currency'; change_output_to: string }
-  | { name: 'change_swap_route'; change_route_to: string }
-  | { name: 'change_input_output' }
   | { name: 'open_account' }
-  | { name: 'open_settings' }
   | { name: 'change_apy'; change_apy_to: number }
-  | { name: 'show_swap_routes' }
   | { name: 'connect'; connect_address: HexAddress; connect_wallet: string }
   | { name: 'connect_click' }
-  | { name: 'open_activity' }
-  | { name: 'change_price_tolerance'; price_tolerance: number };
+  | { name: 'open_activity' };
 
 const analytics = Analytics({
   app: 'oeth-dapp',
