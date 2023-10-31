073eed48a0b5141066df2819d9a3815ca5be9ba2
toniocodo <antoine@originprotocol.com>
2023-10-31 15:14:43 +0100

    feat uniswap v2 routes

--- libs/ousd/swap/src/actions/sushiswap.ts
+++ libs/ousd/swap/src/actions/sushiswap.ts
@@ -1,3 +1,19 @@
+import { contracts, tokens } from '@origin/shared/contracts';
+import { addRatio, isNilOrEmpty } from '@origin/shared/utils';
+import {
+  erc20ABI,
+  getAccount,
+  getPublicClient,
+  prepareWriteContract,
+  readContract,
+  writeContract,
+} from '@wagmi/core';
+import { last } from 'ramda';
+import { formatUnits } from 'viem';
+
+import { GAS_BUFFER } from '../constants';
+
+import type { Token } from '@origin/shared/contracts';
 import type {
   Allowance,
   Approve,
@@ -8,53 +24,274 @@ import type {
   Swap,
 } from '@origin/shared/providers';
 
-const isRouteAvailable: IsRouteAvailable = async () => true;
+const MAX_PRICE = 1.2;
 
-const estimateAmount: EstimateAmount = async ({ amountIn }) => {
-  console.log('Amount estimation not implemented');
+const getPath = (tokenIn: Token, tokenOut: Token) => {
+  if (tokenIn.symbol === tokens.mainnet.OUSD.symbol) {
+    return {
+      [tokens.mainnet.DAI.symbol]: [
+        tokens.mainnet.OUSD.address,
+        tokens.mainnet.USDT.address,
+        tokens.mainnet.DAI.address,
+      ] as const,
+      [tokens.mainnet.USDT.symbol]: [
+        tokens.mainnet.OUSD.address,
+        tokens.mainnet.USDT.address,
+      ] as const,
+      [tokens.mainnet.USDC.symbol]: [
+        tokens.mainnet.OUSD.address,
+        tokens.mainnet.USDT.address,
+        tokens.mainnet.USDC.address,
+      ] as const,
+    }[tokenOut.symbol];
+  } else if (tokenOut.symbol === tokens.mainnet.OUSD.symbol) {
+    return {
+      [tokens.mainnet.DAI.symbol]: [
+        tokens.mainnet.DAI.address,
+        tokens.mainnet.USDT.address,
+        tokens.mainnet.OUSD.address,
+      ] as const,
+      [tokens.mainnet.USDT.symbol]: [
+        tokens.mainnet.USDT.address,
+        tokens.mainnet.OUSD.address,
+      ] as const,
+      [tokens.mainnet.USDC.symbol]: [
+        tokens.mainnet.USDC.address,
+        tokens.mainnet.USDT.address,
+        tokens.mainnet.OUSD.address,
+      ] as const,
+    }[tokenIn.symbol];
+  }
+};
+
+const isRouteAvailable: IsRouteAvailable = async ({
+  amountIn,
+  tokenIn,
+  tokenOut,
+}) => {
+  try {
+    const res = await readContract({
+      address: contracts.mainnet.sushiswapRouter.address,
+      abi: contracts.mainnet.sushiswapRouter.abi,
+      functionName: 'getAmountsOut',
+      args: [amountIn, getPath(tokenIn, tokenOut)],
+    });
+
+    return (
+      +formatUnits(amountIn, tokenIn.decimals) /
+        +formatUnits(last(res), tokenOut.decimals) <
+      MAX_PRICE
+    );
+  } catch {}
+
+  return false;
+};
+
+const estimateAmount: EstimateAmount = async ({
+  amountIn,
+  tokenIn,
+  tokenOut,
+}) => {
+  try {
+    const res = await readContract({
+      address: contracts.mainnet.sushiswapRouter.address,
+      abi: contracts.mainnet.sushiswapRouter.abi,
+      functionName: 'getAmountsOut',
+      args: [amountIn, getPath(tokenIn, tokenOut)],
+    });
+
+    return last(res);
+  } catch {}
 
   return amountIn;
 };
 
-const estimateGas = async () => {
-  console.log('Gas estimation not implemented');
+const estimateGas = async ({
+  tokenIn,
+  tokenOut,
+  amountIn,
+  amountOut,
+  slippage,
+}) => {
+  let gasEstimate = 0n;
 
-  return 0n;
+  if (amountIn === 0n) {
+    return gasEstimate;
+  }
+
+  const publicClient = getPublicClient();
+  const { address } = getAccount();
+
+  const minAmountOut = addRatio(amountOut, tokenOut.decimals, slippage);
+
+  try {
+    gasEstimate = await publicClient.estimateContractGas({
+      address: contracts.mainnet.sushiswapRouter.address,
+      abi: contracts.mainnet.sushiswapRouter.abi,
+      functionName: 'swapExactTokensForTokens',
+      args: [
+        amountIn,
+        minAmountOut,
+        getPath(tokenIn, tokenOut),
+        address,
+        BigInt(Date.now() + 2 * 60 * 1000),
+      ],
+      account: address,
+    });
+  } catch {
+    gasEstimate =
+      tokenIn.symbol === tokens.mainnet.USDT.symbol ||
+      tokenOut.symbol === tokens.mainnet.USDT.symbol
+        ? 175000n
+        : 230000n;
+  }
+
+  return gasEstimate;
 };
 
-const estimateRoute: EstimateRoute = async ({ amountIn, route }) => {
-  console.log('Route estimation not implemented');
+const estimateRoute: EstimateRoute = async ({
+  tokenIn,
+  tokenOut,
+  amountIn,
+  slippage,
+  route,
+}) => {
+  const [estimatedAmount, allowanceAmount, approvalGas] = await Promise.all([
+    estimateAmount({ tokenIn, tokenOut, amountIn }),
+    allowance({ tokenIn, tokenOut }),
+    estimateApprovalGas({ amountIn, tokenIn, tokenOut }),
+  ]);
+  const gas = await estimateGas({
+    tokenIn,
+    tokenOut,
+    amountIn,
+    amountOut: estimatedAmount,
+    slippage,
+  });
 
   return {
     ...route,
-    estimatedAmount: amountIn,
-    allowanceAmount: 0n,
-    approvalGas: 0n,
-    gas: 0n,
-    rate: 0,
+    estimatedAmount,
+    allowanceAmount,
+    approvalGas,
+    gas,
+    rate:
+      +formatUnits(estimatedAmount, tokenOut.decimals) /
+      +formatUnits(amountIn, tokenIn.decimals),
   };
 };
 
-const allowance: Allowance = async () => {
-  console.log('Allowance not implemented');
+const allowance: Allowance = async ({ tokenIn }) => {
+  const { address } = getAccount();
 
-  return 0n;
+  if (isNilOrEmpty(address)) {
+    return 0n;
+  }
+
+  const allowance = await readContract({
+    address: tokenIn.address,
+    abi: erc20ABI,
+    functionName: 'allowance',
+    args: [address, contracts.mainnet.sushiswapRouter.address],
+  });
+
+  return allowance;
 };
 
-const estimateApprovalGas: EstimateApprovalGas = async () => {
-  console.log('Gas approval estimation not implemented');
+const estimateApprovalGas: EstimateApprovalGas = async ({
+  tokenIn,
+  amountIn,
+}) => {
+  let approvalEstimate = 0n;
+  const { address } = getAccount();
 
-  return 0n;
+  if (amountIn === 0n || isNilOrEmpty(address)) {
+    return approvalEstimate;
+  }
+
+  const publicClient = getPublicClient();
+
+  try {
+    approvalEstimate = await publicClient.estimateContractGas({
+      address: tokenIn.address,
+      abi: erc20ABI,
+      functionName: 'approve',
+      args: [contracts.mainnet.sushiswapRouter.address, amountIn],
+      account: address,
+    });
+  } catch {
+    approvalEstimate = 60000n;
+  }
+
+  return approvalEstimate;
 };
 
-const approve: Approve = async () => {
-  console.log('Approve operation not implemented');
-  return null;
+const approve: Approve = async ({ tokenIn, tokenOut, amountIn }) => {
+  const gas = await estimateApprovalGas({
+    amountIn,
+    tokenIn,
+    tokenOut,
+  });
+
+  const { request } = await prepareWriteContract({
+    address: tokenIn.address,
+    abi: erc20ABI,
+    functionName: 'approve',
+    args: [contracts.mainnet.sushiswapRouter.address, amountIn],
+    gas,
+  });
+  const { hash } = await writeContract(request);
+
+  return hash;
 };
 
-const swap: Swap = async () => {
-  console.log('Route swap operation not implemented');
-  return null;
+const swap: Swap = async ({
+  tokenIn,
+  tokenOut,
+  amountIn,
+  slippage,
+  amountOut,
+}) => {
+  const { address } = getAccount();
+
+  if (amountIn === 0n || isNilOrEmpty(address)) {
+    return null;
+  }
+
+  const approved = await allowance({ tokenIn, tokenOut });
+
+  if (approved < amountIn) {
+    throw new Error(`SushiSwap is not approved`);
+  }
+
+  const minAmountOut = addRatio(amountOut, tokenOut.decimals, slippage);
+
+  const estimatedGas = await estimateGas({
+    tokenIn,
+    tokenOut,
+    amountIn,
+    amountOut,
+    slippage,
+  });
+  const gas = estimatedGas + (estimatedGas * GAS_BUFFER) / 100n;
+
+  const { request } = await prepareWriteContract({
+    address: contracts.mainnet.sushiswapRouter.address,
+    abi: contracts.mainnet.sushiswapRouter.abi,
+    functionName: 'swapExactTokensForTokens',
+    args: [
+      amountIn,
+      minAmountOut,
+      getPath(tokenIn, tokenOut),
+      address,
+      BigInt(Date.now() + 2 * 60 * 1000),
+    ],
+    account: address,
+    gas,
+  });
+  const { hash } = await writeContract(request);
+
+  return hash;
 };
 
 export default {
