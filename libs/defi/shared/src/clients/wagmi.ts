1b7e55e8cc0bf6f0df177432167752b2fd778736
toniocodo <antoine@originprotocol.com>
2024-01-15 12:12:54 +0100

    feat: update wagmi config

--- libs/defi/shared/src/clients/wagmi.ts
+++ libs/defi/shared/src/clients/wagmi.ts
@@ -1,74 +1,42 @@
-import { isNilOrEmpty } from '@origin/shared/utils';
-import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-// import { connectorsForWallets } from '@rainbow-me/rainbowkit';
-// import {
-//   argentWallet,
-//   braveWallet,
-//   coinbaseWallet,
-//   injectedWallet,
-//   ledgerWallet,
-//   metaMaskWallet,
-//   rabbyWallet,
-//   rainbowWallet,
-//   safeWallet,
-//   walletConnectWallet,
-// } from '@rainbow-me/rainbowkit/wallets';
-import { fallback, http } from 'wagmi';
+import { connectorsForWallets } from '@rainbow-me/rainbowkit';
+import {
+  coinbaseWallet,
+  ledgerWallet,
+  metaMaskWallet,
+  rabbyWallet,
+  walletConnectWallet,
+} from '@rainbow-me/rainbowkit/wallets';
+import { createConfig, fallback, http } from 'wagmi';
 import { mainnet } from 'wagmi/chains';
 
-// const connectors = connectorsForWallets([
-//   {
-//     groupName: 'Recommended',
-//     wallets: [
-//       metaMaskWallet({
-//         chains,
-//         shimDisconnect: true,
-//         projectId: import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID,
-//       }),
-//       walletConnectWallet({
-//         chains,
-//         projectId: import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID,
-//       }),
-//       coinbaseWallet({ appName: 'Origin DeFi', chains }),
-//       rabbyWallet({ chains }),
-//       braveWallet({ chains, shimDisconnect: true }),
-//     ],
-//   },
-//   {
-//     groupName: 'Others',
-//     wallets: [
-//       injectedWallet({ chains, shimDisconnect: true }),
-//       ledgerWallet({
-//         chains,
-//         projectId: import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID,
-//       }),
-//       safeWallet({ chains }),
-//       rainbowWallet({
-//         chains,
-//         projectId: import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID,
-//       }),
-//       argentWallet({
-//         chains,
-//         projectId: import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID,
-//       }),
-//     ],
-//   },
-// ]);
+const connectors = connectorsForWallets(
+  [
+    {
+      groupName: 'Supported',
+      wallets: [
+        metaMaskWallet,
+        rabbyWallet,
+        walletConnectWallet,
+        coinbaseWallet,
+        ledgerWallet,
+      ],
+    },
+  ],
+  {
+    appName: 'OETH',
+    projectId: import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID,
+  },
+);
 
-export const wagmiConfig = getDefaultConfig({
-  appName: 'Origin DeFi',
-  projectId: import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID,
+export const wagmiConfig = createConfig({
   chains: [mainnet],
+  connectors,
   transports: {
-    [mainnet.id]: isNilOrEmpty(import.meta.env?.VITE_CUSTOM_RPC)
-      ? fallback([
-          http(
-            `${import.meta.env.VITE_ALCHEMY_RPC}${
-              import.meta.env.VITE_ALCHEMY_ID
-            }`,
-          ),
-          http(),
-        ])
-      : http(import.meta.env.VITE_CUSTOM_RPC),
+    [mainnet.id]: fallback([
+      http(
+        `${import.meta.env.VITE_ALCHEMY_RPC}${import.meta.env.VITE_ALCHEMY_ID}`,
+      ),
+      http(),
+    ]),
   },
 });
