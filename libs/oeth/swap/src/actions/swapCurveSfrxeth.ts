196263453cf99c6f1ad94666a3f108fb52e4d97a
toniocodo <antoine@originprotocol.com>
2024-01-18 23:30:22 +0100

    feat: add OETH to sfrxETH curve route

---
+++ libs/oeth/swap/src/actions/swapCurveSfrxeth.ts
@@ -0,0 +1,268 @@
+import { contracts, tokens } from '@origin/shared/contracts';
+import { prepareWriteContractWithTxTracker } from '@origin/shared/providers';
+import {
+  ETH_ADDRESS_CURVE,
+  isNilOrEmpty,
+  subtractSlippage,
+} from '@origin/shared/utils';
+import {
+  erc20ABI,
+  getAccount,
+  getPublicClient,
+  prepareWriteContract,
+  readContract,
+  writeContract,
+} from '@wagmi/core';
+import { formatUnits } from 'viem';
+
+import { GAS_BUFFER } from '../constants';
+
+import type {
+  Allowance,
+  Approve,
+  EstimateAmount,
+  EstimateApprovalGas,
+  EstimateGas,
+  EstimateRoute,
+  Swap,
+} from '@origin/shared/providers';
+
+const config = {
+  routes: [
+    tokens.mainnet.OETH.address,
+    contracts.mainnet.OETHCurvePool.address,
+    ETH_ADDRESS_CURVE,
+    '0xa1f8a6807c402e4a15ef4eba36528a3fed24e577',
+    tokens.mainnet.frxETH.address,
+    tokens.mainnet.sfrxETH.address,
+    tokens.mainnet.sfrxETH.address,
+    '0x0000000000000000000000000000000000000000',
+    '0x0000000000000000000000000000000000000000',
+    '0x0000000000000000000000000000000000000000',
+    '0x0000000000000000000000000000000000000000',
+  ],
+  params: [
+    [1n, 0n, 1n, 1n, 2n],
+    [0n, 1n, 1n, 1n, 2n],
+    [0n, 0n, 8n, 0n, 0n],
+    [0n, 0n, 0n, 0n, 0n],
+    [0n, 0n, 0n, 0n, 0n],
+  ],
+} as const;
+
+const estimateAmount: EstimateAmount = async ({
+  tokenIn,
+  tokenOut,
+  amountIn,
+  curve,
+}) => {
+  if (amountIn === 0n) {
+    return 0n;
+  }
+
+  const amountOut = await readContract({
+    address: contracts.mainnet.CurveRouter.address,
+    abi: contracts.mainnet.CurveRouter.abi,
+    functionName: 'get_dy',
+    args: [config.routes, config.params, amountIn],
+  });
+
+  return amountOut as unknown as bigint;
+};
+
+const estimateGas: EstimateGas = async ({
+  tokenIn,
+  tokenOut,
+  amountIn,
+  curve,
+  amountOut,
+  slippage,
+}) => {
+  let gasEstimate = 0n;
+
+  if (amountIn === 0n) {
+    return gasEstimate;
+  }
+
+  const publicClient = getPublicClient();
+  const { address } = getAccount();
+
+  const minAmountOut = subtractSlippage(amountOut, tokenOut.decimals, slippage);
+
+  try {
+    gasEstimate = await publicClient.estimateContractGas({
+      address: contracts.mainnet.CurveRouter.address,
+      abi: contracts.mainnet.CurveRouter.abi,
+      functionName: 'exchange',
+      args: [config.routes, config.params, amountIn, minAmountOut],
+      account: address ?? ETH_ADDRESS_CURVE,
+    });
+  } catch (e) {
+    gasEstimate = 350000n;
+  }
+
+  return gasEstimate;
+};
+
+const allowance: Allowance = async ({ tokenIn, tokenOut, curve }) => {
+  const { address } = getAccount();
+
+  if (isNilOrEmpty(address)) {
+    return 0n;
+  }
+
+  const allowance = await readContract({
+    address: tokenIn.address,
+    abi: erc20ABI,
+    functionName: 'allowance',
+    args: [address, contracts.mainnet.CurveRouter.address],
+  });
+
+  return allowance;
+};
+
+const estimateApprovalGas: EstimateApprovalGas = async ({
+  tokenIn,
+  amountIn,
+  curve,
+}) => {
+  let approvalEstimate = 0n;
+  const { address } = getAccount();
+
+  if (amountIn === 0n || isNilOrEmpty(address)) {
+    return approvalEstimate;
+  }
+
+  const publicClient = getPublicClient();
+
+  try {
+    approvalEstimate = await publicClient.estimateContractGas({
+      address: tokenIn.address,
+      abi: erc20ABI,
+      functionName: 'approve',
+      args: [contracts.mainnet.CurveRouter.address, amountIn],
+      account: address,
+    });
+  } catch {
+    approvalEstimate = 200000n;
+  }
+
+  return approvalEstimate;
+};
+
+const estimateRoute: EstimateRoute = async ({
+  tokenIn,
+  tokenOut,
+  amountIn,
+  route,
+  slippage,
+  curve,
+}) => {
+  if (amountIn === 0n) {
+    return {
+      ...route,
+      estimatedAmount: 0n,
+      gas: 0n,
+      rate: 0,
+      allowanceAmount: 0n,
+      approvalGas: 0n,
+    };
+  }
+
+  const [estimatedAmount, allowanceAmount, approvalGas] = await Promise.all([
+    estimateAmount({
+      tokenIn,
+      tokenOut,
+      amountIn,
+      curve,
+    }),
+    allowance({ tokenIn, tokenOut, curve }),
+    estimateApprovalGas({ amountIn, tokenIn, tokenOut, curve }),
+  ]);
+  const gas = await estimateGas({
+    tokenIn,
+    tokenOut,
+    amountIn,
+    amountOut: estimatedAmount,
+    slippage,
+    curve,
+  });
+
+  return {
+    ...route,
+    estimatedAmount,
+    gas,
+    approvalGas,
+    allowanceAmount,
+    rate:
+      +formatUnits(estimatedAmount, tokenOut.decimals) /
+      +formatUnits(amountIn, tokenIn.decimals),
+  };
+};
+
+const approve: Approve = async ({ tokenIn, tokenOut, amountIn, curve }) => {
+  const { request } = await prepareWriteContract({
+    address: tokenIn.address,
+    abi: erc20ABI,
+    functionName: 'approve',
+    args: [contracts.mainnet.CurveRouter.address, amountIn],
+  });
+  const { hash } = await writeContract(request);
+
+  return hash;
+};
+
+const swap: Swap = async ({
+  tokenIn,
+  tokenOut,
+  amountIn,
+  amountOut,
+  slippage,
+  curve,
+}) => {
+  const { address } = getAccount();
+
+  if (amountIn === 0n || isNilOrEmpty(address)) {
+    return null;
+  }
+
+  const approved = await allowance({ tokenIn, tokenOut, curve });
+
+  if (approved < amountIn) {
+    throw new Error(`Swap curve is not approved`);
+  }
+
+  const minAmountOut = subtractSlippage(amountOut, tokenOut.decimals, slippage);
+
+  const estimatedGas = await estimateGas({
+    amountIn,
+    slippage,
+    tokenIn,
+    tokenOut,
+    amountOut,
+    curve,
+  });
+  const gas = estimatedGas + (estimatedGas * GAS_BUFFER) / 100n;
+
+  const { request } = await prepareWriteContractWithTxTracker({
+    address: contracts.mainnet.CurveRouter.address,
+    abi: contracts.mainnet.CurveRouter.abi,
+    functionName: 'exchange',
+    args: [config.routes, config.params, amountIn, minAmountOut],
+    account: address,
+    gas,
+  });
+  const { hash } = await writeContract(request);
+
+  return hash;
+};
+
+export default {
+  estimateAmount,
+  estimateGas,
+  estimateRoute,
+  allowance,
+  estimateApprovalGas,
+  approve,
+  swap,
+};
