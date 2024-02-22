648f7a15f5181baef9cc39145bfc4fc25491985a
toniocodo <antoine@originprotocol.com>
2024-02-22 08:57:11 +0100

    feat: add prices rest calls option, update types

--- libs/shared/providers/src/prices/hooks.ts
+++ libs/shared/providers/src/prices/hooks.ts
@@ -1,21 +1,23 @@
-import { isNilOrEmpty } from '@origin/shared/utils';
+import { isFulfilled, isNilOrEmpty } from '@origin/shared/utils';
 import { useQuery } from '@tanstack/react-query';
 import { readContracts } from '@wagmi/core';
 import axios from 'axios';
-import { pickBy } from 'ramda';
+import { groupBy, pickBy } from 'ramda';
 import { useConfig } from 'wagmi';
 
 import { coingeckoApiEndpoint, priceOptions } from './constants';
-import { isCoingeckoCall, isWagmiCall, reduceCalls } from './utils';
+import { isDerivedOption, reduceOptions } from './utils';
 
 import type { UseQueryOptions } from '@tanstack/react-query';
 import type { Config } from '@wagmi/core';
 
 import type {
-  CoingeckoCall,
-  PriceCall,
+  CoingeckoOption,
+  DerivedOption,
+  PriceOption,
+  RestOption,
   SupportedTokenPrice,
-  WagmiCall,
+  WagmiOption,
 } from './types';
 
 export const useTokenPrices = (
@@ -33,53 +35,71 @@ export const useTokenPrices = (
     ...options,
     queryKey: ['useTokenPrices', tokenPrices, config],
     queryFn: async () => {
-      if (isNilOrEmpty(tokenPrices)) {
-        return {} as Record<SupportedTokenPrice, number>;
-      }
-
       const allPrices = {} as Record<SupportedTokenPrice, number>;
 
-      const calls = tokenPrices.reduce(
-        reduceCalls,
-        {} as Record<SupportedTokenPrice, PriceCall>,
+      if (isNilOrEmpty(tokenPrices)) {
+        return allPrices;
+      }
+
+      const options = tokenPrices.reduce(
+        reduceOptions,
+        {} as Record<SupportedTokenPrice, PriceOption>,
       );
 
-      const wagmi = Object.entries(calls).filter(([, v]) => isWagmiCall(v));
-      if (!isNilOrEmpty(wagmi)) {
+      const { wagmi, coingecko, rest } = groupBy(
+        (o) => o.type,
+        Object.values(options),
+      );
+
+      if (wagmi) {
         try {
           const wagmiRes = await readContracts(config, {
             contracts: Object.values(
-              wagmi.map(([, v]) => (v as WagmiCall).config),
+              wagmi.map((o) => (o as WagmiOption).config),
             ),
             allowFailure: true,
           });
           wagmiRes.forEach((res, i) => {
-            const key = wagmi[i][0] as SupportedTokenPrice;
-            const option = priceOptions.mainnet[key];
+            const key = wagmi[i].id;
             allPrices[key] =
-              option?.mapResult?.(res.result) ?? Number(res?.result) ?? 0;
+              (wagmi[i] as WagmiOption)?.mapResult?.(res.result) ??
+              Number(res?.result) ??
+              0;
           });
         } catch {}
       }
 
-      const coingecko = Object.entries(calls).filter(([, v]) =>
-        isCoingeckoCall(v),
-      );
-      if (!isNilOrEmpty(coingecko)) {
+      if (rest) {
+        try {
+          const restRes = await Promise.allSettled(
+            Object.values(rest.map((o) => (o as RestOption).config)),
+          );
+          restRes.forEach((res, i) => {
+            const key = rest[i].id;
+
+            allPrices[key] = isFulfilled(res)
+              ? (rest[i] as RestOption)?.mapResult?.(res.value) ??
+                Number(res.value)
+              : 0;
+          });
+        } catch {}
+      }
+
+      if (coingecko) {
         try {
           const cgRes = await axios.get(
-            `${coingeckoApiEndpoint}/simple/price?ids=${coingecko.map(([, v]) => (v as CoingeckoCall).config).join('%2C')}&vs_currencies=usd`,
+            `${coingeckoApiEndpoint}/simple/price?ids=${coingecko.map((o) => (o as CoingeckoOption).config).join('%2C')}&vs_currencies=usd`,
           );
-          coingecko.forEach(([k, v]) => {
-            allPrices[k as SupportedTokenPrice] =
-              cgRes?.data[v.config as string]?.usd ?? 0;
+          coingecko.forEach((o) => {
+            allPrices[o.id] =
+              cgRes?.data[(o as CoingeckoOption).config]?.usd ?? 0;
           });
         } catch {}
       }
 
       const deps = tokenPrices
-        .map((t) => priceOptions.mainnet[t])
-        .filter((o) => !isNilOrEmpty(o?.dependsOn));
+        .map((t) => priceOptions[t])
+        .filter((o) => isDerivedOption(o)) as DerivedOption[];
       if (!isNilOrEmpty(deps)) {
         try {
           deps.forEach((dep) => {
