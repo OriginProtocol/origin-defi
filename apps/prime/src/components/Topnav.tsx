8a154066ebb2fdd5ec4d9dbe7793caf4993ca230
toniocodo <antoine@originprotocol.com>
2024-02-27 12:02:27 +0100

    feat: add primeETH wallet watch

--- apps/prime/src/components/Topnav.tsx
+++ apps/prime/src/components/Topnav.tsx
@@ -4,16 +4,25 @@ import {
   alpha,
   Box,
   Button,
+  Divider,
   Drawer,
   Link,
+  Popover,
+  Skeleton,
   Stack,
+  Typography,
   useMediaQuery,
   useTheme,
 } from '@mui/material';
 import Grid2 from '@mui/material/Unstable_Grid2/Grid2';
 import { trackEvent } from '@origin/governance/shared';
 import { PauseBanner, usePoints } from '@origin/prime/shared';
-import { LoadingLabel, TokenIcon } from '@origin/shared/components';
+import {
+  ExternalLink,
+  LoadingLabel,
+  TokenIcon,
+  WalletIcon,
+} from '@origin/shared/components';
 import { tokens } from '@origin/shared/contracts';
 import {
   EigenPoints,
@@ -23,27 +32,28 @@ import {
   PrimeStake,
 } from '@origin/shared/icons';
 import {
-  AccountPopover,
+  AddressLabel,
   OpenAccountModalButton,
   useFormat,
+  useWatchBalance,
 } from '@origin/shared/providers';
 import { isNilOrEmpty } from '@origin/shared/utils';
 import { not } from 'ramda';
 import { useIntl } from 'react-intl';
 import { Link as RouterLink, useMatch } from 'react-router-dom';
-import { useAccount } from 'wagmi';
+import { formatUnits } from 'viem';
+import { useAccount, useDisconnect, useWalletClient } from 'wagmi';
 
 import { routes } from '../routes';
 
 import type { BoxProps, StackProps } from '@mui/material';
+import type { Token } from '@origin/shared/contracts';
 import type { RouteObject } from 'react-router-dom';
 
 export function Topnav(props: BoxProps) {
   const theme = useTheme();
   const isSm = useMediaQuery(theme.breakpoints.down('md'));
-  const { isConnected } = useAccount();
-  const [accountModalAnchor, setAccountModalAnchor] =
-    useState<HTMLButtonElement | null>(null);
+
   const [drawerOpen, setDrawerOpen] = useState(false);
 
   return (
@@ -102,35 +112,8 @@ export function Topnav(props: BoxProps) {
               }}
             >
               {!isSm && <PointsBadges mr={2} />}
-              <OpenAccountModalButton
-                onClick={(e) => {
-                  if (isConnected) {
-                    setAccountModalAnchor(e.currentTarget);
-                    trackEvent({
-                      name: 'open_account',
-                    });
-                  } else {
-                    trackEvent({
-                      name: 'connect_click',
-                    });
-                  }
-                }}
-                sx={{
-                  borderRadius: 25,
-                  paddingX: {
-                    md: 3,
-                    xs: 2,
-                  },
-                  paddingY: {
-                    md: 1,
-                    xs: 0.75,
-                  },
-                  minWidth: 36,
-                  maxWidth: { xs: isConnected ? 36 : 160, sm: 160, lg: 220 },
-                  minHeight: { xs: 36, md: 44 },
-                }}
-                connectedProps={{ variant: 'outlined', color: 'secondary' }}
-              />
+              <AccountPopoverButton />
+
               {isSm && (
                 <Button
                   onClick={() => {
@@ -149,10 +132,6 @@ export function Topnav(props: BoxProps) {
                   <FaBarsRegular />
                 </Button>
               )}
-              <AccountPopover
-                anchor={accountModalAnchor}
-                setAnchor={setAccountModalAnchor}
-              />
             </Box>
           </Grid2>
         </Grid2>
@@ -284,3 +263,174 @@ const PointsBadges = (props: StackProps) => {
     </Stack>
   );
 };
+
+const AccountPopoverButton = () => {
+  const intl = useIntl();
+  const theme = useTheme();
+  const [accountModalAnchor, setAccountModalAnchor] =
+    useState<HTMLButtonElement | null>(null);
+  const { address, isConnected, connector } = useAccount();
+  const { disconnect } = useDisconnect();
+  const { data: eth, isLoading: ethLoading } = useWatchBalance();
+  const { data: walletClient } = useWalletClient();
+
+  const close = () => {
+    setAccountModalAnchor(null);
+  };
+
+  const handleAddTokenToWallet = () => {
+    walletClient?.watchAsset({
+      type: 'ERC20',
+      options: tokens.mainnet.primeETH,
+    });
+  };
+
+  return (
+    <>
+      <OpenAccountModalButton
+        connectedProps={{ variant: 'outlined', color: 'secondary' }}
+        onClick={(e) => {
+          if (isConnected) {
+            setAccountModalAnchor(e.currentTarget);
+            trackEvent({
+              name: 'open_account',
+            });
+          } else {
+            trackEvent({
+              name: 'connect_click',
+            });
+          }
+        }}
+        sx={{
+          borderRadius: 25,
+          paddingX: {
+            md: 3,
+            xs: 2,
+          },
+          paddingY: {
+            md: 1,
+            xs: 0.75,
+          },
+          minWidth: 36,
+          maxWidth: { xs: isConnected ? 36 : 160, sm: 160, lg: 220 },
+          minHeight: { xs: 36, md: 44 },
+        }}
+      />
+      <Popover
+        open={!!accountModalAnchor}
+        anchorEl={accountModalAnchor}
+        onClose={close}
+        anchorOrigin={{
+          vertical: 50,
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        sx={{
+          '& .MuiPopover-paper': {
+            borderRadius: 1,
+            width: (theme) => ({
+              xs: '90vw',
+              md: `min(${theme.typography.pxToRem(250)}, 90vw)`,
+            }),
+            [theme.breakpoints.down('md')]: {
+              left: '0 !important',
+              right: 0,
+              marginInline: 'auto',
+            },
+          },
+        }}
+      >
+        <Stack>
+          <Stack
+            justifyContent="space-between"
+            alignItems="center"
+            direction="row"
+            sx={{ px: 2, py: 1.5 }}
+          >
+            <Typography>
+              {intl.formatMessage({ defaultMessage: 'Account' })}
+            </Typography>
+            <Button
+              onClick={() => {
+                disconnect();
+                close();
+              }}
+            >
+              {intl.formatMessage({ defaultMessage: 'Disconnect' })}
+            </Button>
+          </Stack>
+
+          <Divider />
+          <Stack spacing={2} sx={{ px: 2, py: 3 }}>
+            <Stack direction="row" alignItems="center">
+              <WalletIcon
+                walletName={connector?.name}
+                sx={{ width: 20, height: 20, mr: 1.5 }}
+              />
+              <ExternalLink href={`https://etherscan.io/address/${address}`}>
+                <AddressLabel address={address} short />
+              </ExternalLink>
+            </Stack>
+            <Button
+              onClick={handleAddTokenToWallet}
+              variant="outlined"
+              color="secondary"
+            >
+              <TokenIcon
+                symbol={tokens.mainnet.primeETH.symbol}
+                sx={{ fontSize: 20, mr: 1 }}
+              />
+              {intl.formatMessage({
+                defaultMessage: 'Add primeETH',
+              })}
+            </Button>
+          </Stack>
+
+          <Divider />
+          <Stack sx={{ px: 2, py: 3 }} gap={2}>
+            <BalanceRow
+              token={tokens.mainnet.ETH}
+              balance={+formatUnits(eth ?? 0n, 18)}
+              isBalanceLoading={ethLoading}
+            />
+          </Stack>
+        </Stack>
+      </Popover>
+    </>
+  );
+};
+
+type BalanceRowProps = {
+  token: Token;
+  balance: number;
+  isBalanceLoading: boolean;
+} & StackProps;
+
+function BalanceRow({
+  token,
+  balance,
+  isBalanceLoading,
+  ...rest
+}: BalanceRowProps) {
+  const intl = useIntl();
+
+  return (
+    <Stack direction="row" alignItems="center" gap={1} {...rest}>
+      <TokenIcon symbol={token.symbol} sx={{ width: 20, height: 20 }} />
+      <Typography>
+        {isBalanceLoading ? (
+          <Skeleton width={38} />
+        ) : (
+          intl.formatNumber(balance, {
+            minimumFractionDigits: 4,
+            maximumFractionDigits: 4,
+          })
+        )}
+      </Typography>
+      <Typography>{token.symbol}</Typography>
+    </Stack>
+  );
+}
