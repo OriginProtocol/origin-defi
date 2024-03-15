3d48981056255c39dfcc1357309d14e57c03c921
toniocodo <antoine@originprotocol.com>
2024-03-15 14:55:10 +0100

    feat: add protocols full icons

--- libs/shared/icons/src/protocols/index.tsx
+++ libs/shared/icons/src/protocols/index.tsx
@@ -1,17 +1,57 @@
 import { SvgIcon } from '@mui/material';
 
+import AuraFullWebp from './aura-full.webp';
+import BalancerFullSvg from './balancer-full.svg?react';
+import ConvexFullSvg from './convex-full.svg?react';
+import CurveFullWebp from './curve-full.webp';
+import FraxFullSvg from './frax-full.svg?react';
+import LidoFullSvg from './lido-full.svg?react';
+import MorphoFullWebp from './morpho-full.webp';
 import OriginSvg from './origin.svg?react';
+import RocketpoolFullWebp from './rocketpool-full.webp';
 import SnapshotSvg from './snapshot.svg?react';
 import UniswapSvg from './uniswap.svg?react';
 
 import type { SvgIconProps } from '@mui/material';
 
+export const AuraFull = (props: SvgIconProps) => (
+  <SvgIcon {...props} viewBox="0 0 47.3 16">
+    <image href={AuraFullWebp} height="16" width="47.3" />
+  </SvgIcon>
+);
+export const BalancerFull = (props: SvgIconProps) => (
+  <SvgIcon {...props} component={BalancerFullSvg} inheritViewBox />
+);
+export const ConvexFull = (props: SvgIconProps) => (
+  <SvgIcon {...props} component={ConvexFullSvg} inheritViewBox />
+);
+export const CurveFull = (props: SvgIconProps) => (
+  <SvgIcon {...props} viewBox="0 0 59.2 16">
+    <image href={CurveFullWebp} height="16" width="59.2" />
+  </SvgIcon>
+);
+export const FraxFull = (props: SvgIconProps) => (
+  <SvgIcon {...props} component={FraxFullSvg} inheritViewBox />
+);
+export const LidoFull = (props: SvgIconProps) => (
+  <SvgIcon {...props} component={LidoFullSvg} inheritViewBox />
+);
+export const MorphoFull = (props: SvgIconProps) => (
+  <SvgIcon {...props} viewBox="0 0 75.14287 16">
+    <image href={MorphoFullWebp} height="16" width="75.14287" />
+  </SvgIcon>
+);
 export const Origin = (props: SvgIconProps) => (
-  <SvgIcon {...props} component={OriginSvg} viewBox="0 0 22 22" />
+  <SvgIcon {...props} component={OriginSvg} inheritViewBox />
+);
+export const RocketpoolFull = (props: SvgIconProps) => (
+  <SvgIcon {...props} viewBox="0 0 118.16 16">
+    <image href={RocketpoolFullWebp} height="16" width="118.16" />
+  </SvgIcon>
 );
 export const Snapshot = (props: SvgIconProps) => (
-  <SvgIcon {...props} component={SnapshotSvg} viewBox="0 0 16 16" />
+  <SvgIcon {...props} component={SnapshotSvg} inheritViewBox />
 );
 export const Uniswap = (props: SvgIconProps) => (
-  <SvgIcon {...props} component={UniswapSvg} viewBox="40.04 0 470.19 541.43" />
+  <SvgIcon {...props} component={UniswapSvg} inheritViewBox />
 );
