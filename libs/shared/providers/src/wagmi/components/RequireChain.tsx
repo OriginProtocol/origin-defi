072f285a8bc5cc0b5639267272cf121f74a4dfd7
toniocodo <antoine@originprotocol.com>
2024-03-26 13:46:28 +0100

    feat: allow passing list of supported chains

--- libs/shared/providers/src/wagmi/components/RequireChain.tsx
+++ libs/shared/providers/src/wagmi/components/RequireChain.tsx
@@ -5,70 +5,71 @@ import { useAccount, useSwitchChain } from 'wagmi';
 import type { ReactNode } from 'react';
 import type { Chain } from 'viem';
 
+export type RequireChainProps = {
+  sectionName: string;
+  children: ReactNode;
+  allowedChains: Chain[];
+};
+
 export const RequireChain = ({
   sectionName,
   children,
-  chain,
-}: {
-  sectionName: string;
-  children: ReactNode;
-  chain: Chain;
-}) => {
+  allowedChains,
+}: RequireChainProps) => {
   const intl = useIntl();
-  const { chain: currentChain } = useAccount();
+  const { chain } = useAccount();
   const { switchChain } = useSwitchChain();
 
   const handleSwitchNetwork = () => {
-    switchChain({ chainId: chain.id });
+    switchChain({ chainId: allowedChains[0].id });
   };
 
-  const correctChain = currentChain?.id === chain.id;
+  const isChainAllowed = allowedChains
+    .map((c) => c.id)
+    .includes(chain?.id ?? -1);
 
-  let overlay: ReactNode = null;
-  if (currentChain && !correctChain) {
-    overlay = (
-      <Box
-        sx={(theme) => ({
-          position: 'fixed',
-          inset: 0,
-          zIndex: 5,
-          backgroundColor: alpha(theme.palette.background.default, 0.6),
-          backdropFilter: 'blur(15px)',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-        })}
-      >
-        <Stack p={4} spacing={2}>
-          <Typography variant="h1">
-            {intl.formatMessage({ defaultMessage: 'Unsupported Network' })}
-          </Typography>
-          <Typography variant="h3">
-            {intl.formatMessage(
-              {
-                defaultMessage:
-                  'The selected network is not supported by {sectionName}.',
-              },
-              { sectionName },
-            )}
-          </Typography>
-          <Stack direction="row">
-            <Button variant={'action'} onClick={handleSwitchNetwork}>
+  if (chain && !isChainAllowed) {
+    return (
+      <Box sx={{ position: 'relative' }}>
+        <Box
+          sx={(theme) => ({
+            position: 'fixed',
+            inset: 0,
+            zIndex: 5,
+            backgroundColor: alpha(theme.palette.background.default, 0.6),
+            backdropFilter: 'blur(15px)',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          })}
+        >
+          <Stack p={4} spacing={2}>
+            <Typography variant="h1">
+              {intl.formatMessage({ defaultMessage: 'Unsupported Network' })}
+            </Typography>
+            <Typography variant="h3">
               {intl.formatMessage(
-                { defaultMessage: 'Switch to {chain}' },
-                { chain: chain.name },
+                {
+                  defaultMessage:
+                    'The selected network is not supported by {sectionName}.',
+                },
+                { sectionName },
               )}
-            </Button>
+            </Typography>
+            <Stack direction="row">
+              <Button variant={'action'} onClick={handleSwitchNetwork}>
+                {intl.formatMessage(
+                  { defaultMessage: 'Switch to {chain}' },
+                  { chain: chain.name },
+                )}
+              </Button>
+            </Stack>
           </Stack>
-        </Stack>
+        </Box>
+        <Box sx={{ position: 'absolute', top: 0 }}>{children}</Box>
       </Box>
     );
   }
 
-  return (
-    <Box sx={{ position: 'relative' }}>
-      {overlay}
-      <Box sx={{ position: 'absolute', top: 0 }}>{children}</Box>
-    </Box>
-  );
+  return children;
 };
