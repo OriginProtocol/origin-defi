a1f3a1c5e24ff0c0e8219569028bef7263fd2326
Chris Jacobs <apexearth@gmail.com>
2024-04-18 19:54:39 -0700

    feat: pull wOETH balance for all chains

--- libs/oeth/history/src/components/woeth/HistoryHeader.tsx
+++ libs/oeth/history/src/components/woeth/HistoryHeader.tsx
@@ -1,5 +1,5 @@
 import { Divider, Skeleton, Stack, Typography } from '@mui/material';
-import { getTokenBySymbol } from '@origin/shared/contracts';
+import { tokens } from '@origin/shared/contracts';
 import {
   useFormat,
   useTokenPrice,
@@ -10,25 +10,42 @@ import { useIntl } from 'react-intl';
 import { useAccount } from 'wagmi';
 
 import type { StackProps } from '@mui/material';
-import type { Token } from '@origin/shared/contracts';
-import type { erc20Abi } from 'viem';
 
 export function HistoryHeader() {
   const intl = useIntl();
   const { formatAmount } = useFormat();
-  const { address, isConnected, chainId } = useAccount();
-  const wOETH = getTokenBySymbol<Token<typeof erc20Abi>>('wOETH', chainId);
-  const { data: woethBalance, isLoading: oethLoading } = useWatchContract(
-    wOETH && {
-      address: wOETH.address,
-      abi: wOETH.abi,
-      chainId: wOETH.chainId,
-      functionName: 'balanceOf',
-      args: [address ?? ZERO_ADDRESS],
-      query: {
-        enabled: !!address,
+  const { address, isConnected } = useAccount();
+  const woethBalances = [
+    useWatchContract(
+      address && {
+        address: tokens.mainnet.wOETH.address,
+        abi: tokens.mainnet.wOETH.abi,
+        chainId: tokens.mainnet.wOETH.chainId,
+        functionName: 'balanceOf',
+        args: [address ?? ZERO_ADDRESS],
+        query: {
+          enabled: !!address,
+        },
       },
-    },
+    ),
+    useWatchContract(
+      address && {
+        address: tokens.arbitrum.wOETH.address,
+        abi: tokens.arbitrum.wOETH.abi,
+        chainId: tokens.arbitrum.wOETH.chainId,
+        functionName: 'balanceOf',
+        args: [address ?? ZERO_ADDRESS],
+        query: {
+          enabled: !!address,
+        },
+      },
+    ),
+  ];
+
+  const woethLoading = !!woethBalances.find((b) => b.isLoading);
+  const woethBalance = woethBalances.reduce(
+    (sum, b) => sum + (b.data ?? 0n),
+    0n,
   );
   const exchangeRate = useTokenPrice('wOETH_OETH');
 
@@ -43,20 +60,20 @@ export function HistoryHeader() {
     >
       <ValueContainer
         label={intl.formatMessage({ defaultMessage: 'wOETH Balance' })}
-        value={formatAmount(woethBalance, wOETH?.decimals)}
-        isLoading={isConnected && oethLoading}
+        value={formatAmount(woethBalance, tokens.mainnet.wOETH.decimals)}
+        isLoading={isConnected && woethLoading}
       />
       <Divider orientation="vertical" flexItem />
       <ValueContainer
         label={intl.formatMessage({ defaultMessage: 'Current value (OETH)' })}
         value={formatAmount(
           woethBalance,
-          wOETH?.decimals,
+          tokens.mainnet.wOETH.decimals,
           undefined,
           undefined,
           exchangeRate.data,
         )}
-        isLoading={isConnected && oethLoading && exchangeRate.isLoading}
+        isLoading={isConnected && woethLoading && exchangeRate.isLoading}
       />
       <Divider orientation="vertical" flexItem />
     </Stack>
