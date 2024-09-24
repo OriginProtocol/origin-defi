69e0b1c8c3d95d9e88f118085ec5407ce978d88a
toniocodo <antoine@originprotocol.com>
2024-09-25 00:38:01 +0200

    fix: usdc name typo

--- libs/shared/contracts/src/tokens.ts
+++ libs/shared/contracts/src/tokens.ts
@@ -156,7 +156,7 @@ export const tokens = {
       address: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
       chainId: mainnet.id,
       abi: USDCABI,
-      name: 'USD Coin',
+      name: 'USDC Coin',
       decimals: 6,
       symbol: 'USDC',
     },
