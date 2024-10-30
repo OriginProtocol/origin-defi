07705e519d4a28e2bd9256599800b90de6d8dd20
toniocodo <antoine@originprotocol.com>
2024-10-30 19:12:55 +0100

    feat: hide all chains tvl

--- libs/analytics/oeth/src/views/OverviewView.tsx
+++ libs/analytics/oeth/src/views/OverviewView.tsx
@@ -1,8 +1,5 @@
-import { Card, CardContent, Grid2, Stack } from '@mui/material';
 import { Overview } from '@origin/analytics/shared';
-import { ValueLabel } from '@origin/shared/components';
 import { tokens } from '@origin/shared/contracts';
-import { format } from 'dnum';
 import { useIntl } from 'react-intl';
 import { useReadContract } from 'wagmi';
 
@@ -32,7 +29,7 @@ export const OverviewView = () => {
 
   return (
     <Overview token={tokens.mainnet.OETH} currency="ETH">
-      <Grid2 size={12}>
+      {/* <Grid2 size={12}>
         <Stack
           sx={{
             border: '1px solid',
@@ -98,10 +95,10 @@ export const OverviewView = () => {
                 </CardContent>
               </Card>
             </Stack>
-            {/* <OethDistributionCard height={300} /> */}
+            <OethDistributionCard height={300} />
           </Stack>
         </Stack>
-      </Grid2>
+      </Grid2> */}
     </Overview>
   );
 };
