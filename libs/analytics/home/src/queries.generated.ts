33e7f40d6a45fcf7be95d704ff0a87eb1981a6e8
toniocodo <antoine@originprotocol.com>
2024-11-29 00:02:06 +0100

    chore: clean graphql

--- libs/analytics/home/src/queries.generated.ts
+++ libs/analytics/home/src/queries.generated.ts
@@ -16,13 +16,6 @@ export type OgnDailyStatsQueryVariables = Types.Exact<{
 
 export type OgnDailyStatsQuery = { __typename?: 'Query', ognDailyStats: Array<{ __typename?: 'OGNDailyStat', id: string, timestamp: string, priceUSD: number, marketCapUSD: number }> };
 
-export type CumulativeRevenueQueryVariables = Types.Exact<{
-  limit?: Types.InputMaybe<Types.Scalars['Int']['input']>;
-}>;
-
-
-export type CumulativeRevenueQuery = { __typename?: 'Query', oeth: Array<{ __typename?: 'OTokenDailyStat', timestamp: string, blockNumber: number, fees: string, rateETH: string }>, ousd: Array<{ __typename?: 'OTokenDailyStat', timestamp: string, blockNumber: number, fees: string, rateETH: string }>, super: Array<{ __typename?: 'OTokenDailyStat', timestamp: string, blockNumber: number, fees: string, rateETH: string }> };
-
 
 
 export const OgnStatsDocument = `
@@ -96,59 +89,3 @@ useOgnDailyStatsQuery.getKey = (variables?: OgnDailyStatsQueryVariables) => vari
 
 
 useOgnDailyStatsQuery.fetcher = (variables?: OgnDailyStatsQueryVariables, options?: RequestInit['headers']) => graphqlClient<OgnDailyStatsQuery, OgnDailyStatsQueryVariables>(OgnDailyStatsDocument, variables, options);
-
-export const CumulativeRevenueDocument = `
-    query CumulativeRevenue($limit: Int = 5000) {
-  oeth: oTokenDailyStats(
-    limit: $limit
-    orderBy: [timestamp_ASC]
-    where: {otoken_eq: "0x856c4efb76c1d1ae02e20ceb03a2a6a08b0b8dc3", chainId_eq: 1, timestamp_gte: "2023-06-01T00:00:00.000000Z"}
-  ) {
-    timestamp
-    blockNumber
-    fees
-    rateETH
-  }
-  ousd: oTokenDailyStats(
-    limit: $limit
-    orderBy: [timestamp_ASC]
-    where: {otoken_eq: "0x2a8e1e676ec238d8a992307b495b45b3feaa5e86", chainId_eq: 1, timestamp_gte: "2023-06-01T00:00:00.000000Z"}
-  ) {
-    timestamp
-    blockNumber
-    fees
-    rateETH
-  }
-  super: oTokenDailyStats(
-    limit: $limit
-    orderBy: [timestamp_ASC]
-    where: {otoken_eq: "0xdbfefd2e8460a6ee4955a68582f85708baea60a3", chainId_eq: 8453, timestamp_gte: "2024-08-28T00:00:00.000000Z"}
-  ) {
-    timestamp
-    blockNumber
-    fees
-    rateETH
-  }
-}
-    `;
-
-export const useCumulativeRevenueQuery = <
-      TData = CumulativeRevenueQuery,
-      TError = unknown
-    >(
-      variables?: CumulativeRevenueQueryVariables,
-      options?: Omit<UseQueryOptions<CumulativeRevenueQuery, TError, TData>, 'queryKey'> & { queryKey?: UseQueryOptions<CumulativeRevenueQuery, TError, TData>['queryKey'] }
-    ) => {
-    
-    return useQuery<CumulativeRevenueQuery, TError, TData>(
-      {
-    queryKey: variables === undefined ? ['CumulativeRevenue'] : ['CumulativeRevenue', variables],
-    queryFn: graphqlClient<CumulativeRevenueQuery, CumulativeRevenueQueryVariables>(CumulativeRevenueDocument, variables),
-    ...options
-  }
-    )};
-
-useCumulativeRevenueQuery.getKey = (variables?: CumulativeRevenueQueryVariables) => variables === undefined ? ['CumulativeRevenue'] : ['CumulativeRevenue', variables];
-
-
-useCumulativeRevenueQuery.fetcher = (variables?: CumulativeRevenueQueryVariables, options?: RequestInit['headers']) => graphqlClient<CumulativeRevenueQuery, CumulativeRevenueQueryVariables>(CumulativeRevenueDocument, variables, options);
