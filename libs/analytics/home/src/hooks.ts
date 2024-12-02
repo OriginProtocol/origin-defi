1988843c91fbad0dc7010679aca364a26222d8d5
toniocodo <antoine@originprotocol.com>
2024-12-02 20:16:51 +0100

    fix: current date iteration

--- libs/analytics/home/src/hooks.ts
+++ libs/analytics/home/src/hooks.ts
@@ -117,101 +117,83 @@ export const useNetAssetValue = () => {
   return useQuery<NetAssetValue[], Error, NetAssetValue[], ['netAssetValue']>({
     staleTime: Infinity,
     retry: false,
+    throwOnError: true,
     queryKey: ['netAssetValue'],
     queryFn: async () => {
-      try {
-        const res = await Promise.all([
-          axios.get(
-            `https://api.dune.com/api/v1/query/4125829/results?api_key=${import.meta.env.VITE_DUNE_API_KEY}`,
+      const res = await Promise.all([
+        axios.get(
+          `https://api.dune.com/api/v1/query/4125829/results?api_key=${import.meta.env.VITE_DUNE_API_KEY}`,
+        ),
+        queryClient.fetchQuery({
+          queryKey: useTokenChartStats.getKey(
+            tokens.mainnet.OUSD,
+            undefined,
+            minFrom.toISOString(),
           ),
-          queryClient.fetchQuery({
-            queryKey: useTokenChartStats.getKey(
-              tokens.mainnet.OUSD,
-              undefined,
-              minFrom.toISOString(),
-            ),
-            queryFn: useTokenChartStats.fetcher(queryClient),
-          }),
-        ]);
+          queryFn: useTokenChartStats.fetcher(queryClient),
+        }),
+      ]);
 
-        if (!res || !res?.[0]?.data?.length) {
-          return [];
-        }
+      const nav = pathOr([], [0, 'data', 'result', 'rows'], res) as DuneData[];
+      const result: NetAssetValue[] = [];
+      const dailyMap: Record<string, { totalUSD: number; totalETH: number }> =
+        {};
 
-        const nav = pathOr(
-          [],
-          [0, 'data', 'result', 'rows'],
-          res,
-        ) as DuneData[];
-        const result: NetAssetValue[] = [];
-        const dailyMap: Record<string, { totalUSD: number; totalETH: number }> =
-          {};
+      nav.forEach((item: DuneData) => {
+        const date = item.day.substring(0, 10);
+        const ousdStat = res[1].find((stat) => stat.date === date);
+        dailyMap[date] = {
+          totalETH: item.total_usd * (ousdStat?.rateETH ?? 0),
+          totalUSD: item.total_usd,
+        };
+      });
 
-        nav.forEach((item: DuneData) => {
-          const date = item.day.substring(0, 10);
-          const ousdStat = res[1].find((stat) => stat.date === date);
-          dailyMap[date] = {
-            totalETH: item.total_usd * (ousdStat?.rateETH ?? 0),
-            totalUSD: item.total_usd,
-          };
+      const endDate = new Date();
+      let currentDate = minFrom;
+
+      while (currentDate <= endDate) {
+        const dateKey = format(currentDate, 'yyyy-MM-dd');
+        const totalUSD =
+          dailyMap[dateKey]?.totalUSD ??
+          result[result.length - 1]?.totalUSD ??
+          0;
+        const totalETH =
+          dailyMap[dateKey]?.totalETH ??
+          result[result.length - 1]?.totalETH ??
+          0;
+        result.push({
+          timestamp: currentDate.getTime(),
+          totalUSD,
+          totalETH,
         });
-
-        const endDate = new Date();
-        let currentDate = minFrom;
-
-        while (currentDate <= endDate) {
-          const dateKey = format(currentDate, 'yyyy-MM-dd');
-          const totalUSD =
-            dailyMap[dateKey]?.totalUSD ??
-            result[result.length - 1]?.totalUSD ??
-            0;
-          const totalETH =
-            dailyMap[dateKey]?.totalETH ??
-            result[result.length - 1]?.totalETH ??
-            0;
-          result.push({
-            timestamp: new Date(dateKey).getTime(),
-            totalUSD,
-            totalETH,
-          });
-          currentDate = addDays(dateKey, 1);
-        }
-
-        return result;
-      } catch (e) {
-        console.log('Error NAV query ', e);
+        currentDate = addDays(currentDate, 1);
       }
 
-      return [];
+      return result;
     },
     select: useCallback(
       (data: NetAssetValue[]) => {
-        try {
-          let filteredData = data;
+        let filteredData = data;
 
-          if (from || to) {
-            if (from) {
-              filteredData = filteredData.filter(({ timestamp }) => {
-                return (
-                  isSameDay(new Date(timestamp), from) ||
-                  isAfter(new Date(timestamp), from)
-                );
-              });
-            }
-            if (to) {
-              filteredData = filteredData.filter(({ timestamp }) => {
-                return isBefore(new Date(timestamp), subDays(to, 1));
-              });
-            }
-          } else if (limit) {
-            filteredData = takeLast(limit, filteredData);
+        if (from || to) {
+          if (from) {
+            filteredData = filteredData.filter(({ timestamp }) => {
+              return (
+                isSameDay(new Date(timestamp), from) ||
+                isAfter(new Date(timestamp), from)
+              );
+            });
           }
-
-          return filteredData;
-        } catch (e) {
-          console.log('Error NAV mapping ', e);
+          if (to) {
+            filteredData = filteredData.filter(({ timestamp }) => {
+              return isBefore(new Date(timestamp), subDays(to, 1));
+            });
+          }
+        } else if (limit) {
+          filteredData = takeLast(limit, filteredData);
         }
-        return [];
+
+        return filteredData;
       },
       [from, to, limit],
     ),
