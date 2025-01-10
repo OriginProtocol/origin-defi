d6abb5b06b4fb0dba7a8a1d81a504df109b3654e
toniocodo <antoine@originprotocol.com>
2025-01-10 21:39:33 +0100

    fix: use mainnet config for ens

--- libs/shared/providers/src/wagmi/components/UserAvatar.tsx
+++ libs/shared/providers/src/wagmi/components/UserAvatar.tsx
@@ -1,10 +1,16 @@
-import { Box } from '@mui/material';
+import { Box, Skeleton } from '@mui/material';
 import { FaUserRegular } from '@origin/shared/icons';
 import { isNilOrEmpty, ZERO_ADDRESS } from '@origin/shared/utils';
 import { jsNumberForAddress } from 'react-jazzicon';
 import Jazzicon from 'react-jazzicon/dist/Jazzicon';
 import { mainnet } from 'viem/chains';
-import { useAccount, useEnsAvatar, useEnsName } from 'wagmi';
+import {
+  createConfig,
+  http,
+  useAccount,
+  useEnsAvatar,
+  useEnsName,
+} from 'wagmi';
 
 import type { BoxProps } from '@mui/material';
 import type { HexAddress } from '@origin/shared/utils';
@@ -14,22 +20,31 @@ export type UserAvatarProps = { address?: HexAddress; width?: number } & Omit<
   'width'
 >;
 
+const wagmiConfig = createConfig({
+  chains: [mainnet],
+  transports: {
+    [mainnet.id]: http(),
+  },
+});
+
 export const UserAvatar = ({ address, ...rest }: UserAvatarProps) => {
   const { address: self } = useAccount();
   const adr = address ?? self;
-  const { data: ensName } = useEnsName({
+  const { data: ensName, isLoading: ensNameLoading } = useEnsName({
     address: adr,
     chainId: mainnet.id,
     query: {
       enabled: !isNilOrEmpty(adr),
     },
+    config: wagmiConfig,
   });
-  const { data: ensAvatar } = useEnsAvatar({
+  const { data: ensAvatar, isLoading: ensAvatarLoading } = useEnsAvatar({
     name: ensName ?? undefined,
     chainId: mainnet.id,
     query: {
       enabled: !!ensName,
     },
+    config: wagmiConfig,
   });
 
   if (isNilOrEmpty(adr)) {
@@ -46,6 +61,18 @@ export const UserAvatar = ({ address, ...rest }: UserAvatarProps) => {
         ]}
       >
         <FaUserRegular sx={{ fontSize: rest?.width ?? 24 }} />
+      </Box>
+    );
+  }
+
+  if (ensNameLoading || ensAvatarLoading) {
+    return (
+      <Box {...rest}>
+        <Skeleton
+          variant="circular"
+          width={rest?.width ?? 24}
+          height={rest?.width ?? 24}
+        />
       </Box>
     );
   }
