67ea873ebb6d13ed0124b9edb2db66b8cf6d103d
toniocodo <antoine@originprotocol.com>
2025-04-01 09:12:55 +0200

    fix: remove unused open ocean route

--- libs/defi/ogn/src/swap/actions.ts
+++ libs/defi/ogn/src/swap/actions.ts
@@ -1,4 +1,4 @@
-import { magpie, openOcean } from '@origin/shared/routes';
+import { magpie } from '@origin/shared/routes';
 import { defineMessage } from 'react-intl';
 
 import type { SwapApi } from '@origin/shared/providers';
@@ -9,11 +9,6 @@ export const ognSwapActions: Record<OgnSwapAction, SwapApi> = {
   'swap-magpie-ogn': {
     ...magpie,
     routeLabel: defineMessage({ defaultMessage: 'Swap via Magpie' }),
-    buttonLabel: defineMessage({ defaultMessage: 'Swap' }),
-  },
-  'swap-openOcean-ogn': {
-    ...openOcean,
-    routeLabel: defineMessage({ defaultMessage: 'Swap via OpenOcean' }),
     buttonLabel: defineMessage({ defaultMessage: 'Swap' }),
   },
 };
