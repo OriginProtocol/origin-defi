6be6907448126ac5208d3502081d339b6decc90d
toniocodo <antoine@originprotocol.com>
2025-04-02 20:36:24 +0200

    refactor: update usePendingYield hook with chainId

--- libs/defi/shared/src/hooks/usePendingYield.ts
+++ libs/defi/shared/src/hooks/usePendingYield.ts
@@ -2,12 +2,13 @@ import { contracts, tokens } from '@origin/shared/contracts';
 import { hasKey } from '@origin/shared/utils';
 import { useQuery } from '@tanstack/react-query';
 import { readContracts } from '@wagmi/core';
-import { formatEther } from 'viem';
+import { add, div, eq, from, mul, sub, toNumber } from 'dnum';
 import { useAccount, useConfig } from 'wagmi';
 
 import type { Token } from '@origin/shared/contracts';
 import type { HexAddress } from '@origin/shared/utils';
 import type { QueryOptions } from '@tanstack/react-query';
+import type { Dnum } from 'dnum';
 
 const supportedContracts = {
   [tokens.mainnet.OETH.id]: {
@@ -65,43 +66,50 @@ export const usePendingYield = (
               address: supportedContracts[token.id].vault.address,
               abi: supportedContracts[token.id].vault.abi,
               functionName: 'totalValue',
+              chainId: supportedContracts[token.id].vault.chainId,
             },
             {
               address: token.address,
               abi: token.abi,
               functionName: 'totalSupply',
+              chainId: token.chainId,
             },
             {
               address: supportedContracts[token.id].dripper.address,
               abi: supportedContracts[token.id].dripper.abi,
               functionName: 'availableFunds',
+              chainId: supportedContracts[token.id].dripper.chainId,
             },
             {
               address: token.address,
               abi: token.abi,
               functionName: 'nonRebasingSupply',
+              chainId: token.chainId,
             },
             {
               address: token.address,
               abi: token.abi,
               functionName: 'balanceOf',
               args: [address],
+              chainId: token.chainId,
             },
           ],
         })
       ).map((res) =>
         res.status === 'success'
-          ? +formatEther(res?.result as unknown as bigint)
-          : 0,
+          ? ([res?.result, token.decimals] as Dnum)
+          : from(0, token.decimals),
       );
 
-      const vaultYield = totalValue - totalSupply;
-      const expectedYield = vaultYield + availableFunds;
-      const rebasingSupply = totalSupply - nonRebasingSupply;
-      const expectedYieldPerOeth = expectedYield / rebasingSupply;
-      const expectedYieldPerOethWithFee = expectedYieldPerOeth * 0.8;
+      const vaultYield = sub(totalValue, totalSupply);
+      const expectedYield = add(vaultYield, availableFunds);
+      const rebasingSupply = sub(totalSupply, nonRebasingSupply);
+      const expectedYieldPerOeth = eq(rebasingSupply, 0)
+        ? 0
+        : div(expectedYield, rebasingSupply);
+      const expectedYieldPerOethWithFee = mul(expectedYieldPerOeth, 0.8);
 
-      return balance * expectedYieldPerOethWithFee;
+      return toNumber(mul(balance, expectedYieldPerOethWithFee));
     },
     ...options,
   });
